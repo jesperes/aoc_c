874fcb9d4dfa78b7286a88bcce79478ef76249da
Jesper Eskilson <jesper.eskilson@klarna.com>
2022-01-01 19:00:17 +0100

    day 19 wip

---
+++ src/2021/day19.c
@@ -0,0 +1,10 @@
+#include "aoc.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+aoc_result_t day19(const char *input, int len) {
+    aoc_result_t result = {0};
+    return result;
+}
